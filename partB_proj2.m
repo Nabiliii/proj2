968321a7d7968c6b097915cc620fbcd68d808b95
Nabiliii <113493094+Nabiliii@users.noreply.github.com>
2023-04-23 13:41:39 +1000

    Add files via upload

--- partB_proj2.m
+++ partB_proj2.m
@@ -1,115 +1,106 @@
 
 function Main()
 clc;clear;
-
-% Load data, to be played back. 
-file='DataUsr_p021.mat';   
-% load the data, now.
+file='DataUsr_p020.mat';   
 load(file); 
-% Use the data.
 ExploreData(data);
 end
-% ----------------------------------------
+% -------------------------------------------------------------------------
+
 function ExploreData(data)
-%AA=API_MTRN4010a(1);      % init API, JUST once
-
-X_0             = data.pose0;            %platform's initial pose; [x0;y0;heading0] [meters;meters;radians]
-n_events        = data.n ;               % how many events?
-table           = data.table;            % table of events.
-event0          = table(:,1);            % first event.
-t0              = event0(1);             % initial time (the time of event0).
-t0              = 0.0001*double(t0);     % "event" is of integer type, that is why we convert t0 to double ( i.e. to "real") and over to s
-vw              = [0;0];                 % The last [speed,heading rate] measurement.
-
-
-%............................
-Lidar1Cfg    = data.LidarsCfg.Lidar1;  %Info about LiDAR installation (position and orientation,in platform's coordinate frame.). 
-%............................
-
-%initial som variables:
-measured_phi = [];
-X_kin_mod = zeros(2,n_events);
-X_kin_mod_correct = zeros(2,n_events);
-for i = 1:n_events % can use for loop but then buttons do not work.
-
-            %pos_buffer(:,i) = X_0;  %record current pose; so, we can analyze the estimated trajectory after the loop ends.
+    fprintf('Estimating Bias... \n\n');
+    
+    X_0             = data.pose0;            %platform's initial pose; [x0;y0;heading0] [meters;meters;radians]
+    n_events        = data.n ;               % how many events?
+    table           = data.table;            % table of events.
+    event0          = table(:,1);            % first event.
+    t0              = event0(1);             % initial time (the time of event0).
+    t0              = 0.0001*double(t0);     % "event" is of integer type, that is why we convert t0 to double ( i.e. to "real") and over to s
+    vw              = [0;0];                 % The last [speed,heading rate] measurement.
+    Lidar1Cfg    = data.LidarsCfg.Lidar1;  %Info about LiDAR installation (position and orientation,in platform's coordinate frame.). 
+    
+    %initial som variables:
+    measured_phi = [];
+    X_kin_mod = zeros(2,n_events);
+    X_kin_mod_correct = zeros(2,n_events);
+    
+    for i = 1:n_events % can use for loop but then buttons do not work.
+    
+                %pos_buffer(:,i) = X_0;  %record current pose; so, we can analyze the estimated trajectory after the loop ends.
+                % get event description.
+                event     = table(:,i);                %event #i    -->(:,i) gives me columns                   
+                sensorID  = event(3);
+                index     = event(2);                  % where to read the actual measurement, from that sensor recorder.% source (i.e. which sensor generated this event?)
+                
+                tNow      = 0.0001*double(event(1));   % when was this measurement taken? Time in tNow is expressed in seconds.
+                dt        = tNow-t0;                   % dt since last event ( "dt" is needed for prediction steps).
+                t0        = tNow;                      % remember current time, so we can calculate dt in next iteration.            
+                
+                
+                X_0             = MyKinematicModel(X_0,vw,dt); 
+                X_kin_mod(1,i)  = X_0(1);
+                X_kin_mod(2,i)  = X_0(2);
+                theta           = X_0(3);
+                % Calculate angular velocity using diff function
+                
+                measured_phi = [measured_phi; theta];
+                
+                 % measurement is from which sensor?
+                 switch sensorID   
+                    
+                    case 1         %  it is a scan from  LiDAR#1, and scan from LiDAR#2! (both, as they are synchronized)
+    
+                    % both LiDARs are sinchronized.
+                    scan1 = data.scans(:,index);  %get the col of scan1, each col is a scan (301 scans).
+    
+                   pause(0.005);
+                   continue;                     
+                    
+                    %...............................
+                    case 2                          %  It is speed encoder + gyro  (they are packed together, synchonized readings)
+                    
+                    vw   = data.vw(:,index);        % speed and gyroZ, last updated copy. keep the variable "vw" updated to the last reading of those sensors.
+                   % i_when_vw_input = [i_when_vw_input; i];
+                    continue;
+                 end 
+    end
+    
+    %***********************************************
+    %get the measurements after the loop
+    measured_phi = measured_phi(1:17:end);
+    gt = data.verify.poseL(3,:);
+    gt = gt';
+    % Run the optimization
+    bias_initial_guess = 0.1;
+    bias_estimate = estimate_bias(bias_initial_guess,measured_phi,gt);
+    fprintf('Estimated bias: %.3f deg/s \n\n',bias_estimate);
+    disp ('Running vehicle with estimated bias...');
+    %***********************************************
+    
+    %new loop for running vehicle with corrected angular rate:
+    X_1             = data.pose0;            %platform's initial pose; [x0;y0;heading0] [meters;meters;radians]
+    n_events1        = data.n ;               % how many events?
+    table1           = data.table;            % table of events.
+    %event1          = table(:,1);            % first event.
+    t1              = event0(1);             % initial time (the time of event0).
+    t1              = 0.0001*double(t1);     % "event" is of integer type, that is why we convert t0 to double ( i.e. to "real") and over to s
+    vw1              = [0;0];  
+    plot_envirnment = InitCertainPartOfMyProgram(data); %figure 11 -background 
+    for i = 1:n_events1 % can use for loop but then buttons do not work.
             % get event description.
-            event     = table(:,i);                %event #i    -->(:,i) gives me columns                   
-            sensorID  = event(3);
-            index     = event(2);                  % where to read the actual measurement, from that sensor recorder.% source (i.e. which sensor generated this event?)
+            event1     = table1(:,i);                %event #i    -->(:,i) gives me columns                   
+            sensorID  = event1(3);
+            index     = event1(2);                  % where to read the actual measurement, from that sensor recorder.% source (i.e. which sensor generated this event?)
             
-            tNow      = 0.0001*double(event(1));   % when was this measurement taken? Time in tNow is expressed in seconds.
-            dt        = tNow-t0;                   % dt since last event ( "dt" is needed for prediction steps).
-            t0        = tNow;                      % remember current time, so we can calculate dt in next iteration.            
+            tNow1      = 0.0001*double(event1(1));   % when was this measurement taken? Time in tNow is expressed in seconds.
+            dt        = tNow1-t1;                   % dt since last event ( "dt" is needed for prediction steps).
+            t1        = tNow1;                      % remember current time, so we can calculate dt in next iteration.            
             
-            
-            X_0             = MyKinematicModel(X_0,vw,dt); 
-            X_kin_mod(1,i)  = X_0(1);
-            X_kin_mod(2,i)  = X_0(2);
-            theta           = X_0(3);
-            % Calculate angular velocity using diff function
-            
-            measured_phi = [measured_phi; theta];
-            
-             % measurement is from which sensor?
-             switch sensorID   
-                
-                case 1         %  it is a scan from  LiDAR#1, and scan from LiDAR#2! (both, as they are synchronized)
-
-                % both LiDARs are sinchronized.
-                scan1 = data.scans(:,index);  %get the col of scan1, each col is a scan (301 scans).
-
-               pause(0.005);
-               continue;                     
-                
-                %...............................
-                case 2                          %  It is speed encoder + gyro  (they are packed together, synchonized readings)
-                
-                vw   = data.vw(:,index);        % speed and gyroZ, last updated copy. keep the variable "vw" updated to the last reading of those sensors.
-               % i_when_vw_input = [i_when_vw_input; i];
-                continue;
-             end 
-end
-
-
-X_1             = data.pose0;            %platform's initial pose; [x0;y0;heading0] [meters;meters;radians]
-n_events        = data.n ;               % how many events?
-table           = data.table;            % table of events.
-event0          = table(:,1);            % first event.
-t0              = event0(1);             % initial time (the time of event0).
-t0              = 0.0001*double(t0);     % "event" is of integer type, that is why we convert t0 to double ( i.e. to "real") and over to s
-vw              = [0;0];       
-
-measured_phi = measured_phi(1:17:end);
-
-gt = data.verify.poseL(3,:);
-gt = gt';
-
-bias_initial_guess = 0.1;
-
-% Run the optimization
-bias_estimate = estimate_bias(bias_initial_guess,measured_phi,gt);
-fprintf('Estimated bias: %.3f deg/s\n',bias_estimate);
-
-
-
-for i = 1:n_events % can use for loop but then buttons do not work.
-            % get event description.
-            event     = table(:,i);                %event #i    -->(:,i) gives me columns                   
-            sensorID  = event(3);
-            index     = event(2);                  % where to read the actual measurement, from that sensor recorder.% source (i.e. which sensor generated this event?)
-            
-            tNow      = 0.0001*double(event(1));   % when was this measurement taken? Time in tNow is expressed in seconds.
-            dt        = tNow-t0;                   % dt since last event ( "dt" is needed for prediction steps).
-            t0        = tNow;                      % remember current time, so we can calculate dt in next iteration.            
-            
-            
-            X_1             = MyKinematicModel_corrected(X_1,vw,dt,bias_estimate); 
+            X_1             = MyKinematicModel_corrected(X_1,vw1,dt,bias_estimate); 
             X_kin_mod_correct(1,i)  = X_1(1);
             X_kin_mod_correct(2,i)  = X_1(2);
             phi           = X_1(3);
             
-           
             %the UGV current pos, part_C:
                 % length of the arrow
                 L = 2;
@@ -117,36 +108,32 @@ for i = 1:n_events % can use for loop but then buttons do not work.
                 u = L*cos(phi);
                 v = L*sin(phi);
                 %plot the ugv dynamicly with a arrow sticking out:
-            % set(plot_envirnment,'xdata',X_1(1),'ydata',X_1(2),'UData',u,'VData',v);
+                set(plot_envirnment(1),'xdata',X_1(1),'ydata',X_1(2),'UData',u,'VData',v);
 
              % measurement is from which sensor?
              switch sensorID   
-                
                case 1         %  it is a scan from  LiDAR#1, and scan from LiDAR#2! (both, as they are synchronized)
-                    %fprintf('Event (LiDAR), dt=[%.1fms], at t=[%.3f]\n',dt*1000,tNow);   % print some info, for debugging our code.
-                    
-                    % both LiDARs are sinchronized.
-                    scan1 = data.scans(:,index);  %get the col of scan1, each col is a scan (301 scans).
-                
-                    pause(0.005);
-               continue;                     
-                    case 2                          %  It is speed encoder + gyro  (they are packed together, synchonized readings)
-                    vw   = data.vw(:,index);        % speed and gyroZ, last updated copy. keep the variable "vw" updated to the last reading of those sensors.
+                   scan1 = data.scans(:,index);  %get the col of scan1, each col is a scan (301 scans).
+
+                   processLiD_cartiesian_plot(plot_envirnment(2),scan1,X_1,MyKinematicModel_corrected(X_1,vw1,dt,bias_estimate),Lidar1Cfg,1);                                                         
+                   pause(0.005);
+                   continue;                     
+                case 2                          %  It is speed encoder + gyro  (they are packed together, synchonized readings)
+                    vw1   = data.vw(:,index);        % speed and gyroZ, last updated copy. keep the variable "vw" updated to the last reading of those sensors.
                     continue;
              end 
-end
-
-plot_envirnment = InitCertainPartOfMyProgram(data); %figure 11 -background 
-ShowVerification1(data,X_kin_mod,X_kin_mod_correct);%,global_pos_OOI1,OOIs_center,lm, OOI );
+    end
+    
+    ShowVerification1(data,X_kin_mod,X_kin_mod_correct);%,global_pos_OOI1,OOIs_center,lm, OOI );
+    plot_for_bias(data,X_kin_mod,X_kin_mod_correct);
 
 end  
-
 
 % -----------------------------------------------------------------------------------------------------------
 function plot_envirnment = InitCertainPartOfMyProgram(data)
 
     % create some figure for your stuff.
-    figure(4); clf();    % global CF.
+    figure(1); clf();    % global CF.
     
     Landmarks=data.Context.Landmarks;
     % plot centres of landmarks. 
@@ -158,7 +145,7 @@ function plot_envirnment = InitCertainPartOfMyProgram(data)
     hold on;
     Walls = data.Context.Walls;
     plot(Walls(1,:),Walls(2,:),'color',[0,1,0]*0.7,'linewidth',3);
-    legend({'Centers of landmarks','Walls (middle planes) '});
+    legend({'Centers of landmarks','Walls (middle planes)' });%', 'pos0','car','GT','X','wrong X'});
     
     title('Global CF (you should show some results here)');
     xlabel('X (m)'); 
@@ -171,7 +158,7 @@ function plot_envirnment = InitCertainPartOfMyProgram(data)
 end
 % ---------------------------------------------------------------------------------
 function ShowVerification1(data,X_kin_mod,MyKinematicModel_corrected)%,global_pos1,OOIs_center,matchedPoints1, matchedPoints2 )
-figure(4)
+figure(1)
 hold on;
 p=data.verify.poseL;
 plot(p(1,:),p(2,:),'r.');
@@ -184,7 +171,7 @@ plot(X_kin_mod(1,:),X_kin_mod(2,:), 'c.','MarkerSize',0.5);
 %plot (global_pos1(:,1),global_pos1(:,2),'b+');
 
 hold on;
-plot(MyKinematicModel_corrected(1,:),MyKinematicModel_corrected(2,:), 'b.','MarkerSize',0.5); 
+plot(MyKinematicModel_corrected(1,:),MyKinematicModel_corrected(2,:), 'b.','MarkerSize',1); 
 
 
 end
@@ -192,17 +179,22 @@ end
 
 function figur_handles = CreateFigureToShowScansInPolar()
    
-    figure(4); 
+    figure(1); 
+    movegui(gcf, [70 400]);
+
     hold on;
-    movegui(gcf, [80 230]);
-    hold on;
-    h = quiver(0,0,'O','LineWidth', 2,'MaxHeadSize', 0.5,'MarkerSize',15,'MarkerFaceColor','r','MarkerEdgeColor','k');
+    h1 = quiver(0,0,'O','LineWidth', 2,'MaxHeadSize', 0.5,'MarkerSize',15,'MarkerFaceColor','r','MarkerEdgeColor','k');
     %h5 = quiver(0,0,'s','LineWidth', 1,'MarkerFaceColor','r','MarkerEdgeColor','k');
     title('OOIs globl CF');  xlabel('X (m)');  ylabel('Y (m)'); axis([-5,21,-5,26]); grid on;
+    hold on;  
+    h2 = plot(0,0,'m.','MarkerSize',1);     %dots from lidar1             %10
+    
+    figure(2);
+    hold on;
+    title('Part E Bias');xlabel('X (m)');  ylabel('Y (m)'); axis([-5,20,-5,20]);grid on;
+     movegui(gcf, [640 200]);
    
-
-   
-    figur_handles = h;
+    figur_handles = [h1,h2];
 end    
 % ---------------------------------------------------------------------------------
 
@@ -223,7 +215,68 @@ function X = MyKinematicModel_corrected(X,vw,dt,bias)
        X(3) = X(3) + vw_tun(2) * dt;
 end  
 % ---------------------------------------------------------------------------------
+function  processLiD_cartiesian_plot(figure_handle,scan,T,X_kin_mod, LidarCfg,id) %plot scans and OOI in cartes. LiDaR_CF and global
+  
+    
+        %plot dynamicaly the scans in cartesian in global_CF:
+        [scans_global_pos] = scan_local_to_global(scan,id, T,X_kin_mod,LidarCfg);
+       set(figure_handle,'xdata',scans_global_pos(:,1), 'ydata',scans_global_pos(:,2));
 
+
+end
+
+% ---------------------------------------------------------------------------------
+function [x,y] = Ranges_in_cartesian_LiDaR_CF(scan)   %used in two fuctions above, returns pos of dots in cartesan local CF
+    mask1  = 16383;  
+    ranges = single(bitand(scan,mask1))*0.01; 
+    angles = [-75:0.5:75]';
+    angles = deg2rad(angles);
+    % Convert to Cartesian coordinates using the LiDAR's geometry
+    x = ranges .* cos(angles);
+    y = ranges .* sin(angles);
+end
+
+% ---------------------------------------------------------------------------------
+function [scans_global_pos] = scan_local_to_global(scan,id, T,X_kin_mod_correct,LidarCfg) %Retrun the all dots in Global_CF
+    %the heading of the vehicle:
+    theta = T(3);
+    % lidar position in GCF (assuming scalar):
+    x_origin = X_kin_mod_correct(1); 
+    y_origin = X_kin_mod_correct(2); 
+    %Lidar pos in UGV:
+    lx = LidarCfg.Lx;
+    ly = LidarCfg.Ly;
+%-----------------------------
+    % alpha =  LidarCfg.Alpha for lidar1 and + some corrector for lidar2:
+    if id == 1
+        alpha = LidarCfg.Alpha;
+    end
+    if id == 2
+        tun = corrct_angle();   % =5 for p020  and -6 for p2021
+        alpha = LidarCfg.Alpha + deg2rad(tun);
+    end
+%-----------------------------
+
+    %the rotation matrix:
+    R = [cos(theta) -sin(theta);
+        sin(theta) cos(theta)];
+    
+    %alpha = 3.1416;
+    R_LiDaR= [cos(alpha) -sin(alpha);
+              sin(alpha) cos(alpha)];
+    
+    % now get the scans (dots) in local CF using detect_ooi function
+    [x,y] = Ranges_in_cartesian_LiDaR_CF(scan);  %return scan_pos in cartesian local LiDaR_CF (X_l,y_l)
+    p_local = zeros(301,2);
+    for i = 1: size(x)
+        p_local(i,:) = R_LiDaR * [x(i),y(i)]'+[lx;ly]; 
+    end
+   
+    scans_global_pos = R * p_local' + [x_origin;y_origin ];
+    scans_global_pos = scans_global_pos'; %//was wrong dim. global pos give n*1 while shoud be n*2
+end
+
+% ---------------------------------------------------------------------------------
 
 function c = Cost(Bias,measured_phi , GR_thruth_phi)
  
@@ -247,3 +300,22 @@ function  bias_estimate = estimate_bias(bias_initial_guess,measured_phi,GR_thrut
     
 
 end
+% ---------------------------------------------------------------------------------
+
+function plot_for_bias(data,X_kin_mod,X_kin_mod_correct)
+    figure(2)
+    hold on;
+    p=data.verify.poseL;
+    plot(p(1,:),p(2,:),'r.','markersize',1);
+    %legend({'Landmarks','Walls (middle planes)','Initial pose','Ground truth (subsampled)'});
+    hold on;
+    plot(X_kin_mod_correct(1,:),X_kin_mod_correct(2,:), 'k-'); %plot the data from the kinematic model
+
+    hold on;
+    plot(X_kin_mod(1,:),X_kin_mod(2,:), 'c-','LineWidth',2); %plot the data from the kinematic model
+
+    legend({'ground Truth','kin. model corrected','kin. model biased'});
+   
+ end
+
+
